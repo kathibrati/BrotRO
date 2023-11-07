ca3aaae4e98e2656b434bd13b0a04dc1f92a9849
Kathleen Letsch <cskathi@web.de>
2023-11-07 18:24:20 +0100

    Try Fix Rogue Backstab Direction

--- src/map/skill.cpp
+++ src/map/skill.cpp
@@ -5422,9 +5422,24 @@ int skill_castend_damage_id (struct block_list* src, struct block_list *bl, uint
 
 				if (battle_check_target(src, bl, BCT_ENEMY) > 0 && unit_movepos(src, bl->x + x, bl->y + y, 2, true)) { // Display movement + animation.
 #else
-				uint8 dir = map_calc_dir(src, bl->x, bl->y), t_dir = unit_getdir(bl);
+				uint8 dir = map_calc_dir(src, bl->x, bl->y);
+				short x, y;
 
-				if (!map_check_dir(dir, t_dir) || bl->type == BL_SKILL) {
+				if (dir > 0 && dir < 4)
+					x = -1;
+				else if (dir > 4)
+					x = 1;
+				else
+					x = 0;
+
+				if (dir > 2 && dir < 6)
+					y = -1;
+				else if (dir == 7 || dir < 2)
+					y = 1;
+				else
+					y = 0;
+
+				if (battle_check_target(src, bl, BCT_ENEMY) > 0 && unit_movepos(src, bl->x + x, bl->y + y, 2, true)) { // Display movement + animation.
 #endif
 					status_change_end(src, SC_HIDING);
 					dir = dir < 4 ? dir+4 : dir-4; // change direction [Celest]
