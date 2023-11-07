4000307ab4cd81af0614af52e11aad54ab2dca73
Kathleen Letsch <cskathi@web.de>
2023-11-07 22:40:34 +0100

    Try fix netcode for posibugs

--- src/map/unit.cpp
+++ src/map/unit.cpp
@@ -40,7 +40,7 @@
 
 using namespace rathena;
 
-#ifndef MAX_SHADOW_SCAR 
+#ifndef MAX_SHADOW_SCAR
 	#define MAX_SHADOW_SCAR 100 /// Max Shadow Scars
 #endif
 
@@ -233,10 +233,10 @@ int unit_check_start_teleport_timer(struct block_list *sbl)
 	int max_dist = 0;
 
 	switch(sbl->type) {
-		case BL_HOM:	
-		case BL_ELEM:	
-		case BL_PET:	
-		case BL_MER:	
+		case BL_HOM:
+		case BL_ELEM:
+		case BL_PET:
+		case BL_MER:
 			msd = unit_get_master(sbl);
 			break;
 		default:
@@ -314,7 +314,7 @@ TIMER_FUNC(unit_step_timer){
 	//Execute request now if target is in attack range
 	if(ud->stepskill_id && skill_get_inf(ud->stepskill_id) & INF_GROUND_SKILL) {
 		//Execute ground skill
-		struct map_data *md = &map[bl->m];			
+		struct map_data *md = &map[bl->m];
 		unit_skilluse_pos(bl, target_id%md->xs, target_id/md->xs, ud->stepskill_id, ud->stepskill_lv);
 	} else {
 		//If a player has target_id set and target is in range, attempt attack
@@ -366,7 +366,7 @@ int unit_walktoxy_ontouch(struct block_list *bl, va_list ap)
 		break;
 	// case BL_MOB:	// unsupported
 	}
-	
+
 	return 0;
 }
 
@@ -384,7 +384,7 @@ static TIMER_FUNC(unit_walktoxy_timer)
 
 	if(bl == nullptr)
 		return 0;
-	
+
 	unit_data *ud = unit_bl2ud(bl);
 
 	if(ud == nullptr)
@@ -438,7 +438,7 @@ static TIMER_FUNC(unit_walktoxy_timer)
 	int y = bl->y;
 
 	//Monsters will walk into an icewall from the west and south if they already started walking
-	if(map_getcell(bl->m,x+dx,y+dy,CELL_CHKNOPASS) 
+	if(map_getcell(bl->m,x+dx,y+dy,CELL_CHKNOPASS)
 		&& (icewall_walk_block == 0 || dx < 0 || dy < 0 || !map_getcell(bl->m,x+dx,y+dy,CELL_CHKICEWALL)))
 		return unit_walktoxy_sub(bl);
 
@@ -594,7 +594,7 @@ static TIMER_FUNC(unit_walktoxy_timer)
 
 	if(ud->state.change_walk_target) {
 		if(unit_walktoxy_sub(bl)) {
-			return 1;	
+			return 1;
 		} else {
 			clif_fixpos(bl);
 			return 0;
@@ -1022,7 +1022,7 @@ int unit_escape(struct block_list *bl, struct block_list *target, short dist, ui
  * @param bl: Object to instant warp
  * @param dst_x: X coordinate to warp to
  * @param dst_y: Y coordinate to warp to
- * @param easy: 
+ * @param easy:
  *		0: Hard path check (attempt to go around obstacle)
  *		1: Easy path check (no obstacle on movement path)
  *		2: Long path check (no obstacle on line from start to destination)
@@ -1460,7 +1460,7 @@ int unit_is_walking(struct block_list *bl)
 	return (ud->walktimer != INVALID_TIMER);
 }
 
-/** 
+/**
  * Checks if a unit is able to move based on status changes
  * View the StatusChangeStateTable in status.cpp for a list of statuses
  * Some statuses are still checked here due too specific variables
@@ -1864,7 +1864,11 @@ int unit_skilluse_id2(struct block_list *src, int target_id, uint16 skill_id, ui
 	if(ud->stepaction || ud->steptimer != INVALID_TIMER)
 		unit_stop_stepaction(src);
 	// Remember the skill request from the client while walking to the next cell
-	if(src->type == BL_PC && ud->walktimer != INVALID_TIMER && !battle_check_range(src, target, range-1)) {
+	//if(src->type == BL_PC && ud->walktimer != INVALID_TIMER && !battle_check_range(src, target, range-1)) {
+	if(src->type == BL_PC && !battle_check_range(src, target, range)) {
+    	if (ud->walktimer == INVALID_TIMER) {
+    		unit_walktoxy(src, target->x, target->y, 8);
+    	}
 		ud->stepaction = true;
 		ud->target_to = target_id;
 		ud->stepskill_id = skill_id;
@@ -2295,7 +2299,7 @@ int unit_set_target(struct unit_data* ud, int target_id)
 	if( ud->target != target_id ) {
 		struct unit_data * ux;
 		struct block_list* target;
-	
+
 		if (ud->target && (target = map_id2bl(ud->target)) && (ux = unit_bl2ud(target)) && ux->target_count > 0)
 			ux->target_count--;
 
@@ -2403,7 +2407,7 @@ int unit_unattackable(struct block_list *bl)
  * Requests a unit to attack a target
  * @param src: Object initiating attack
  * @param target_id: Target ID (bl->id)
- * @param continuous: 
+ * @param continuous:
  *		0x1 - Whether or not the attack is ongoing
  *		0x2 - Whether function was called from unit_step_timer or not
  * @return Success(0); Fail(1);
@@ -2456,14 +2460,18 @@ int unit_attack(struct block_list *src,int target_id,int continuous)
 	if(ud->stepaction || ud->steptimer != INVALID_TIMER)
 		unit_stop_stepaction(src);
 	// Remember the attack request from the client while walking to the next cell
-	if(src->type == BL_PC && ud->walktimer != INVALID_TIMER && !battle_check_range(src, target, range-1)) {
+	//if(src->type == BL_PC && ud->walktimer != INVALID_TIMER && !battle_check_range(src, target, range-1)) {
+	if(src->type == BL_PC && !battle_check_range(src, target, range)) {
+    	if (ud->walktimer == INVALID_TIMER) {
+    		unit_walktoxy(src, target->x, target->y, 8);
+    	}
 		ud->stepaction = true;
 		ud->target_to = ud->target;
 		ud->stepskill_id = 0;
 		ud->stepskill_lv = 0;
 		return 0; // Attacking will be handled by unit_walktoxy_timer in this case
 	}
-	
+
 	if(DIFF_TICK(ud->attackabletime, gettick()) > 0) // Do attack next time it is possible. [Skotlex]
 		ud->attacktimer=add_timer(ud->attackabletime,unit_attack_timer,src->id,0);
 	else // Attack NOW.
@@ -2472,7 +2480,7 @@ int unit_attack(struct block_list *src,int target_id,int continuous)
 	return 0;
 }
 
-/** 
+/**
  * Cancels an ongoing combo, resets attackable time, and restarts the
  * attack timer to resume attack after amotion time
  * @author [Skotlex]
@@ -2743,6 +2751,7 @@ static int unit_attack_timer_sub(struct block_list* src, int tid, t_tick tick)
 
 	if(sd && !check_distance_client_bl(src,target,range)) {
 		// Player tries to attack but target is too far, notify client
+		clif_fixpos(src);  // synchronize the player's position with the client
 		clif_movetoattack(sd,target);
 		return 1;
 	} else if(md && !check_distance_bl(src,target,range)) {
@@ -2784,7 +2793,7 @@ static int unit_attack_timer_sub(struct block_list* src, int tid, t_tick tick)
 			// Set mob's ANGRY/BERSERK states.
 			md->state.skillstate = md->state.aggressive?MSS_ANGRY:MSS_BERSERK;
 
-			if (status_has_mode(sstatus,MD_ASSIST) && DIFF_TICK(tick, md->last_linktime) >= MIN_MOBLINKTIME) { 
+			if (status_has_mode(sstatus,MD_ASSIST) && DIFF_TICK(tick, md->last_linktime) >= MIN_MOBLINKTIME) {
 				// Link monsters nearby [Skotlex]
 				md->last_linktime = tick;
 				map_foreachinrange(mob_linksearch, src, md->db->range2, BL_MOB, md->mob_id, target, tick);
